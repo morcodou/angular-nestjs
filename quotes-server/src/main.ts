432c22442da8dfc051b1acd7c75c484afc730ba7
Apple <apple@MBP.local>
2020-04-09 14:37:18 -0400

    debug lunch node

--- quotes-server/src/main.ts
+++ quotes-server/src/main.ts
@@ -7,7 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
+  const options = new DocumentBuilder() 
     .setTitle('Quotes API')
     .setDescription('Quotes API Description')
     .setVersion('0.1')
