e42c7efc2a4e7f52a40ce5a93e3ed93c4e03ed81
Apple <apple@MBP.local>
2020-04-09 15:04:02 -0400

    adding mongodb

--- quotes-server/src/app.module.ts
+++ quotes-server/src/app.module.ts
@@ -3,9 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesController } from './quotes/quotes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/quotes', {
+      useNewUrlParser: true,
+    })
+  ],
   controllers: [
     AppController,
     QuotesController
