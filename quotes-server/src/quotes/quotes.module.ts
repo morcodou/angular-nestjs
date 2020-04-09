2f7e2a13e9f11ed2e1c87ceac0c72730dddb88c6
Apple <apple@MBP.local>
2020-04-09 15:16:32 -0400

    add schema quote

--- quotes-server/src/quotes/quotes.module.ts
+++ quotes-server/src/quotes/quotes.module.ts
@@ -1,20 +1,22 @@
+import { QuoteSchema } from './schemas/quote.schema';
 import { QuotesController } from './quotes.controller';
 import { Module } from '@nestjs/common';
 import { QuotesService } from './quotes.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [
-    //   MongooseModule.forRoot(process.env.MONGO_URL, {
-    //     useNewUrlParser: true,
-    //   }),
-    ],
-    controllers: [
-      QuotesController
-    ],
-    providers: [
-      QuotesService,
-    ],
-  })
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Quote', schema: QuoteSchema },
+    ]),
+  ],
+  controllers: [
+    QuotesController
+  ],
+  providers: [
+    QuotesService,
+  ],
+})
 export class QuotesModule {
 
 }
