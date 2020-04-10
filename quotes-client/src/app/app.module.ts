60a9ddc5177e3a0a45079828df8a5518773463e5
Apple <apple@MBP.local>
2020-04-09 21:39:39 -0400

    quote service api

--- quotes-client/src/app/app.module.ts
+++ quotes-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule,
 
   ],
   providers: [],
